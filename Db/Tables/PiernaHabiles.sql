6e3714ac61ad6783bd6cb1e1e331d0b368c2956a
ezeklug <ezeklug@gmail.com>
2025-03-12 22:02:32 -0300

    feat: agrego db

---
+++ Db/Tables/PiernaHabiles.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PiernaHabiles]
+(
+    Id int primary key identity (1,1),
+	Descripcion varchar(50) not null
+)
--- src/Infrastructure/Data/Migrations/20250312232509_CreateIdentitySchema2.Designer.cs
+++ src/Infrastructure/Data/Migrations/20250312232509_CreateIdentitySchema2.Designer.cs
@@ -4,6 +4,7 @@ using EstadisticasIndependiente.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EstadisticasIndependiente.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250312232509_CreateIdentitySchema2")]
+    partial class CreateIdentitySchema2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
